f20b1ae85e8f6988892c6e0230e29f8304c51cce
David Castellanos Zayas <david383812@gmail.com>
2023-02-25 10:32:23 -0500

    terminando cambios en el sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className='flex'>
-          <Sidebar/>
+          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+            <Sidebar/>
+          </div>
 
           {/* ClientProvider - Notification */}
-          
+
           <div className='bg-[#343541] flex-1'>{children}</div>
         </div>
       </body>
