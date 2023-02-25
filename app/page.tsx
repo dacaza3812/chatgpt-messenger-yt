e2bf797b70ca9d9255c9479db77c328f70b4b5b5
David Castellanos Zayas <david383812@gmail.com>
2023-02-25 09:38:00 -0500

    Hasta ahora se tiene la primera parte de los ejemplos

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+        <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+        <div>
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    {/* Sun Icon */}
+                    <h2>Examples</h2>
+                </div>
+
+                <div className="space-y-2">
+                    <p className="infoText">"Explain Something to me"</p>
+                    <p className="infoText">"What is the diference between a dog and a cat"</p>
+                    <p className="infoText">"What is the color of the sun"</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
