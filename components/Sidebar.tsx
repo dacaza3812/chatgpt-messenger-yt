d785a3cc345baceb9d8a9f4e10dc4d9f8c5c52ce
dacaza3812 <51470264+dacaza3812@users.noreply.github.com>
2023-02-27 13:32:09 -0500

    Se construllo en componente chat row

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { collection } from "firebase/firestore";
 import {useSession, signOut} from "next-auth/react";
 import {useCollection} from 'react-firebase-hooks/firestore';
 import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 import NewChat from "./NewChat";
 
 function Sidebar() {
